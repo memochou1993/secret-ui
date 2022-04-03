101d6040941f73aaedd9ff5401177574f742054a
Memo Chou <memochou1993@gmail.com>
2022-04-03 22:34:32 +0800

    Update wording

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -125,7 +125,7 @@ export default function Login() {
             mt: 2,
           }}
         >
-          Incorrect username or password.
+          Incorrect email address or password.
         </Alert>
       )}
       <Copyright
