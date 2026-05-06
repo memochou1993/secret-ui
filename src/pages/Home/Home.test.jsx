9ba7848636cf675ff84298deac71e1b3457459f1
Memo Chou <memochou1993@gmail.com>
2026-05-06 13:56:02 +0800

    Remove react-scripts and testing setup

--- src/pages/Home/Home.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
-
-test('', () => {
-  render(<Home />);
-  const element = screen.getByText();
-  expect(element).toBeInTheDocument();
-});
